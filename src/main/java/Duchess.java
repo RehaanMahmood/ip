c53ed85f25ecdc18abe47f77fe7a9276e87d41ae
Rehaan Mahmood <rehaanmd.310506@gmail.com>
2026-01-29 22:09:05 +0800

    Add echo functionality

--- src/main/java/Duchess.java
+++ src/main/java/Duchess.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duchess {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Duchess.");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hey, I'm Duchess.");
+        System.out.println("     How can I help you?");
+        System.out.println("    ____________________________________________________________\n");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input =  scanner.nextLine();
+            System.out.println("    ____________________________________________________________");
+            if (input.equals("bye")) {
+                System.out.println("     Bye, see you.");
+                System.out.println("    ____________________________________________________________");
+                break;
+            } else {
+                System.out.println("     " + input);
+                System.out.println("    ____________________________________________________________\n");
+            }
+        }
     }
 }
