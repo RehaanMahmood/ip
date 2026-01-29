6e6dccc6e716b9e73b574d4d99a4bfafc3a4d19a
Rehaan Mahmood <rehaanmd.310506@gmail.com>
2026-01-29 22:19:19 +0800

    Add add and list functionality

--- src/main/java/Duchess.java
+++ src/main/java/Duchess.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duchess {
@@ -7,6 +8,8 @@ public class Duchess {
         System.out.println("     How can I help you?");
         System.out.println("    ____________________________________________________________\n");
         Scanner scanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int count = 0;
         while (true) {
             String input =  scanner.nextLine();
             System.out.println("    ____________________________________________________________");
@@ -15,9 +18,17 @@ public class Duchess {
                 System.out.println("    ____________________________________________________________");
                 break;
             } else {
-                System.out.println("     " + input);
+                list[count] = input;
+                count++;
+                printList(Arrays.copyOf(list, count));
                 System.out.println("    ____________________________________________________________\n");
             }
+        }
+    }
+
+    public static void printList(String[] list) {
+        for (int i = 0; i < list.length; i++) {
+            System.out.println("     " + (i + 1) + ". " + list[i]);
         }
     }
 }
